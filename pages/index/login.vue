b1ed1adf09d40e36b460ce1e6fecdae809403f26
cj <877570089@qq.com>
2024-01-10 16:20:25 +0800

    1

--- pages/index/login.vue
+++ pages/index/login.vue
@@ -3,3 +3,26 @@
     	
     </view>
 </template>
+<script>
+	export default {
+		data() {
+			return {
+				falg:false,
+				ico:[
+					'../../static/by.png',
+					'../../static/zy.png'
+				],
+				userInfo:{
+					name:'',
+					pwd:'',
+				}
+			}
+		},
+		methods: {
+			changeFlag(){
+				this.flag=!this.flag
+			},
+			
+	}
+}
+</script>
