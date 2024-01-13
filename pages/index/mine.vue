957f38d00138ac09024b48c8178a97c9c5bb5228
cj <877570089@qq.com>
2024-01-13 11:27:55 +0800

    1

---
+++ pages/index/mine.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<view class="header">
+			<h1>欢迎您:{{user.username}}</h1>
+			<h2>电话：{{user.phoneNumber}}</h2>
+		</view>
+	</view>
+</template>
+import 
+<script>
+	export default{
+		data(){
+			return{
+				user:{
+					username:'cj',
+					password:'23',
+					phoneNumber:'111'
+				}
+			}
+		},
+		method:{
+			
+		}
+	}
+</script>
+<style>
+.header{
+	width: 100vw;
+	height:40vh;
+}
+.header h1{
+	text-align: center;
+}
+.header h2{
+	bottom: 0;
+	text-align: center;
+}
+</style>
